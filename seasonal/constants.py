3dbb184732d3a76f85c4c71998b65d5a98d674a9
Karen McKinnon <mckinnon@fas.harvard.edu>
2021-05-10 16:13:21 -0600

    added omega

--- seasonal/constants.py
+++ seasonal/constants.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # from CMIP5 mean estimates in Geoffroy et al 2013
 rho = 1030  # kg/m3
 cp = 4180  # J/kg/K
@@ -8,3 +10,4 @@ C_ocean_deep = 774*mwe
 gamma = 0.74
 seconds_per_day = 60*60*24
 days_per_year = 365.25
+omega = 2*np.pi/(days_per_year*seconds_per_day)
